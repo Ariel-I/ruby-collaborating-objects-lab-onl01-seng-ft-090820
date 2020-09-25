27e5f74e87f55eea8f99479bc8788ceb45bb9fd7
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-25 05:28:40 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,22 +22,9 @@ class Artist
   end
 
   def self.find_or_create_by_name(name) 
-    if self.find(name)
-      self.find(name)
-    else
-      self.create(name)
-    end
-  end
-
-  def self.find(name)     
-    self.all.detect { |artist| artist.name == name }
-  end
-
-  def self.create(name)   
-    artist = Artist.new(name)
-    artist.save
-    artist
-  end
+    self.all.detect { |artist| artist.name == name } || Artist.new(name)
+  end 
+  
 
   def print_songs 
    self.songs.each { |song| puts song.name }
