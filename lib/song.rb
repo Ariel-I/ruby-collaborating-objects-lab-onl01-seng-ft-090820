e30e0b0a4efcea5ceebf27f819b2301da5928f04
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-25 05:25:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,10 +17,7 @@ class Song
     @@all
   end
 
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end
+
 
   def self.new_by_filename(file)
     song_info = file.chomp(".mp3").split(" - ")
